eedaa0b0d274ac146ae1a73469283c82c71e18be
Dan Palmieri <dlspalmieri@gmail.com>
2023-02-18 20:31:40 -0300

    pint some code

--- src/LaravelBrazilianPortugueseServiceProvider.php
+++ src/LaravelBrazilianPortugueseServiceProvider.php
@@ -7,15 +7,15 @@ use Illuminate\Support\ServiceProvider;
 class LaravelBrazilianPortugueseServiceProvider extends ServiceProvider
 {
     /**
-    * Publishes translation files.
-    *
-    * @return  void
-    */
+     * Publishes translation files.
+     *
+     * @return  void
+     */
     public function register()
     {
         $this->publishes([
-            __DIR__ . '/pt_BR.json' => $this->app->langPath() . '/pt_BR.json',
-            __DIR__ . '/pt_BR' => $this->app->langPath() . '/pt_BR',
+            __DIR__.'/pt_BR.json' => $this->app->langPath().'/pt_BR.json',
+            __DIR__.'/pt_BR' => $this->app->langPath().'/pt_BR',
         ], 'laravel-brazilian-portuguese');
     }
 }
